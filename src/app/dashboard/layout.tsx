7e4777f90d5bab767d58a7e3e8a6442d542c6132
IlonaKuzmina-GWM <ilona@greenwiremedia.com>
2024-10-15 12:06:44 +0300

    small layout fix

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -2,24 +2,19 @@ import RoleSwitcher from "@/components/RoleSwitcher";
 import SiderBar from "@/components/shared/SideBar/SideBar";
 import React from "react";
 
-interface ILayout {
-  children: React.ReactNode;
-}
-
 export default function DasboardLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <div className={`flex h-svh bg-bgWhite`}>
+    <div className={`relative flex h-svh bg-bgWhite`}>
       <SiderBar />
       <main className="flex w-full flex-row overflow-y-auto px-4 py-4 xl:px-10">
         {children}
       </main>
-      <div className="absolute right-0 top-1/2">
-        <RoleSwitcher />
-      </div>
+
+      <RoleSwitcher />
     </div>
   );
 }

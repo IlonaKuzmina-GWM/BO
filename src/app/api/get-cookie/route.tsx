3fda4eef7635407b3b37e8e20772cda06422e4a4
IlonaKuzmina-GWM <ilona@greenwiremedia.com>
2024-10-30 15:02:13 +0200

    deploy error fix

--- src/app/api/get-cookie/route.tsx
+++ src/app/api/get-cookie/route.tsx
@@ -3,7 +3,7 @@ import { cookies } from "next/headers";
 
 
 export async function GET(request: NextRequest) {
-  const cookiesStore = cookies();
+  const cookiesStore = await cookies();
   const token = cookiesStore.get("authToken")?.value;
   const userId = cookiesStore.get("userId")?.value;
 

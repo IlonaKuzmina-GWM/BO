6793b2ef54eac9d521dcf30ba9565aa9b0bd3b9e
nesj <alexanmiro@gmail.com>
2024-11-29 17:07:48 +0200

    fix

--- src/app/auth/verify/[token]/page.tsx
+++ src/app/auth/verify/[token]/page.tsx
@@ -2,26 +2,18 @@
 
 import { useStore } from "@/stores/StoreProvider";
 import { useEffect, useState } from "react";
-import React from "react";
 
 const centerStyles = "h-screen flex justify-center items-center";
-const inputStyles = "mb-5 bg-[#e9e8e8] p-5 rounded-[20px] text-black"
+const inputStyles = "mb-5 bg-[#e9e8e8] p-5 rounded-[20px] text-black";
 
-export default function VerifyTokenPage({ params }: { params: { token: string } }) {
-    const { alertStore } = useStore();
+export default function VerifyTokenPage() {
+    const url = new URL(window.location.href);
+    const splittedPath = url.pathname.split('/');
+  const { alertStore } = useStore();
   const [isValidToken, setIsValidToken] = useState<boolean | null>(null);
-  const [token, setToken] = useState<string | null>(null);
+  const [token, setToken] = useState<string | null>(splittedPath[splittedPath.length - 1] || null);
   const [password, setPassword] = useState<string>("");
   const [repeatPassword, setRepeatPassword] = useState<string>("");
-
-  //@ts-ignore
-  const resolvedParams = React.use<{ token: string }>(params);
-
-  useEffect(() => {
-    if (resolvedParams && resolvedParams.token) {
-      setToken(resolvedParams.token);
-    }
-  }, [resolvedParams]);
 
   useEffect(() => {
     if (token) {
@@ -33,17 +25,20 @@ export default function VerifyTokenPage({ params }: { params: { token: string } 
               "Content-Type": "application/json",
             },
           });
-  
+
           if (response.ok) {
             console.log("Response", response);
+            setIsValidToken(true);
           } else {
             console.log("Response failed");
+            setIsValidToken(false);
           }
         } catch (e: any) {
-          console.error('Error while checking token: ', e.message);
+          console.error("Error while checking token: ", e.message);
+          setIsValidToken(false);
         }
       };
-  
+
       validateToken();
     }
   }, [token]);
@@ -58,18 +53,36 @@ export default function VerifyTokenPage({ params }: { params: { token: string } 
 
   const handleButton = () => {
     if (!password || !repeatPassword) {
-        // alertStore.setAlert("error", `Please, provide password and repeat password`);
-    };
-  }
+      alertStore.setAlert("error", `Please, provide password and repeat password`);
+    }
+  };
 
   return (
     <div className={`${centerStyles} text-center flex flex-col`}>
-        <h1 className="text-[40px] mb-[20px]">Setup your password</h1>
-        <div className="flex flex-col">
-            <input type="password" className={`${inputStyles}`} placeholder="Password" />
-            <input type="password" className={`${inputStyles}`} placeholder="Repeat password" />
-        </div>
-        <button onClick={handleButton} className="bg-[#e9e8e8] text-black rounded-[20px] py-[5px] px-[30px] text-[20px] transition-all hover:scale-95" type="submit">Finish</button>
+      <h1 className="text-[40px] mb-[20px]">Setup your password</h1>
+      <div className="flex flex-col">
+        <input
+          type="password"
+          className={`${inputStyles}`}
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <input
+          type="password"
+          className={`${inputStyles}`}
+          placeholder="Repeat password"
+          value={repeatPassword}
+          onChange={(e) => setRepeatPassword(e.target.value)}
+        />
+      </div>
+      <button
+        onClick={handleButton}
+        className="bg-[#e9e8e8] text-black rounded-[20px] py-[5px] px-[30px] text-[20px] transition-all hover:scale-95"
+        type="submit"
+      >
+        Finish
+      </button>
     </div>
   );
 }
